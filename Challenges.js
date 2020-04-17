a33ea796eb06a6883b017aa8abfeec9c85124486
Garry <garrypierre@Garrys-MacBook-Pro.local>
2020-04-16 20:34:38 -0400

    up dating file

--- Challenges.js
+++ Challenges.js
@@ -108,9 +108,7 @@ function init() {
     document.querySelector('.player-0-panel').classList.add('active');
 }
 
-//document.querySelector('#current-' + activePlayer).textContent = dice;
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-//var x = document.querySelector('#score-0').textContent;
+
 
 
 
